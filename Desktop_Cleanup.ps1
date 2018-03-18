74f4a20cffa4b2807159b9d25953c62e09e3bb4f
Drew Anderson <andrew@andersontech.org>
2018-03-18 14:27:39 -0400

    Init

---
+++ Desktop_Cleanup.ps1
@@ -0,0 +1,41 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID b31bac00-cfd7-4c22-b92c-370d0c6ec8d2
+
+.AUTHOR Drew Anderson
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Cleans up shortcuts on the user's desktop as well as the Public Desktop 
+
+#> 
+Param()
+
+
