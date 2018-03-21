3c6b0fb3f5a0de92f3466080091237854ff178c8
Drew Anderson <andrew@andersontech.org>
2018-03-21 15:38:42 -0400

    Modified comment

--- LastBootTime.ps1
+++ LastBootTime.ps1
@@ -1,6 +1,6 @@
-# This function is intended to be used to check the last boot time of a system. I picked this up
-# a while ago and don't recall where I got it. If this is yours or you know where it originated,
-# please let me know
+# This function is intended to be used to check the last boot time of a system. I've had this
+# sitting around for some time now and don't recall how much of it I wrote myself and how much
+# I found on stackoverflow. Please use this however you wish.
 Function Get-LastBootTime ($ComputerName) {
     $LastBootUpTime = Get-WmiObject Win32_OperatingSystem -ComputerName $ComputerName | Select-Object -ExpandProperty LastBootUpTime
     [System.Management.ManagementDateTimeConverter]::ToDateTime($LastBootUpTime)
