c5d88819d068c10dc07abb95611f9ae1e4287071
unknown <andrew@andersontech.org>
2018-12-01 15:08:33 -0500

    Mostly working script

--- Windows/RemoveDuplicatePictures.ps1
+++ Windows/RemoveDuplicatePictures.ps1
@@ -1,1 +1,13 @@
 # To remove duplicate picture and video files from a given directory
+
+$rootpath = "D:\Pictures"
+
+# Collect only directories one level under the root path
+$subdirectories = Get-ChildItem -Path $rootpath -Directory
+
+foreach ($dir in $subdirectories.FullName) {
+    $dupespath = New-Item -Path $dir -Name "Duplicates" -ItemType Directory -Confirm:$false
+    $items = Get-ChildItem -Path $dir
+    $dupes = $items | Where-Object {$_.Name -like "*(*"}
+    $dupes | Move-Item -Destination $dupespath -Confirm:$false
+}
