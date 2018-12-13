bc01e4f7f48f3534a7d638a1c58d92abc6927b7c
unknown <andrew@andersontech.org>
2018-12-12 22:36:41 -0500

    Command to change boot timeout

---
+++ Windows/ChangeBootTimeout.bat
@@ -0,0 +1,5 @@
+REM This command can be run to change the boot timeout delay that allows for OS selection. This
+REM can be useful on a Windows Server Core server that does not have the GUI to update.
+REM The following command sets OS boot timeout to 5 seconds.
+
+bcdedit /timeout 5
